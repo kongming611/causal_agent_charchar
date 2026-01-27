dfdc763863bd525e1c587a3039d5aab309b14bdf
kongming611 <2170736183@qq.com>
2026-01-27 19:55:15 +0800

    对类的基础应用

---
+++ day2_oop.py
@@ -0,0 +1,31 @@
+class StudentAgent:
+    def __init__(self, name):
+        self.name = name
+        self.scores = []
+        self.history = []
+        print(f"智能体 {self.name} 初始化完成！")
+
+    def add_score(self, score):
+        self.scores.append(score)
+        print(f"{self.name} 录入成绩 {score}")
+
+    def speak(self, content):
+        self.history.append(content)
+        print(f"{self.name} 说: {content}")
+
+    def recall(self):
+        print(f"\n --- 开始读取 {self.name} 的记忆 ---")
+        for i, msg in enumerate(self.history):
+            print(f"记忆片段 {i + 1}: {msg}")
+
+agent = StudentAgent("钟江铭")
+
+agent.speak("你好，我是材料类学生创作的Agent！")
+agent.speak("我刚刚弄懂了Python的类和对象。")
+
+agent.add_score(85)
+agent.add_score(95)
+
+agent.recall()
+
+print(f"最后一次成绩是 {agent.scores[-1]}")
