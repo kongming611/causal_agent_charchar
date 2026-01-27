dbbe704593d995e3bdedd178ecb9b7b9a7431817
kongming611 <2170736183@qq.com>
2026-01-27 21:24:03 +0800

    对类的基础应用

--- day2_oop.py
+++ day2_oop.py
@@ -1,6 +1,7 @@
 class StudentAgent:
-    def __init__(self, name):
+    def __init__(self, name, major):
         self.name = name
+        self.major = major
         self.scores = []
         self.history = []
         print(f"智能体 {self.name} 初始化完成！")
@@ -11,14 +12,15 @@ class StudentAgent:
 
     def speak(self, content):
         self.history.append(content)
-        print(f"{self.name} 说: {content}")
+        print(f"{self.major} {self.name} 说: {content}")
 
     def recall(self):
         print(f"\n --- 开始读取 {self.name} 的记忆 ---")
         for i, msg in enumerate(self.history):
             print(f"记忆片段 {i + 1}: {msg}")
 
-agent = StudentAgent("钟江铭")
+agent = StudentAgent("钟江铭", "材料类")
+agent2 = StudentAgent("蕾蕾", "应用心理学")
 
 agent.speak("你好，我是材料类学生创作的Agent！")
 agent.speak("我刚刚弄懂了Python的类和对象。")
@@ -26,6 +28,11 @@ agent.speak("我刚刚弄懂了Python的类和对象。")
 agent.add_score(85)
 agent.add_score(95)
 
+agent2.add_score(100)
+agent2.speak("我是蕾蕾，我考了满分！")
+
 agent.recall()
 
-print(f"最后一次成绩是 {agent.scores[-1]}")
+# print(f"最后一次成绩是 {agent.scores[-1]}")
+print(f"{agent.major} {agent.name} 的分数列表: {agent.scores}")
+print(f"蕾蕾的分数列表: {agent2.scores}")
